04ab9abcd73c71490a77f0dcedd365c0ffa19e22
Graham Livingston <gramlivingston@gmail.com>
2021-09-01 13:44:27 -0500

    fastapi and jinja2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # main.py
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,15 +7,18 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == '__main__':
+    uvicorn.run(app)
 
 
 
